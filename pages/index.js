e1d6e32719f55f04939342040eb75e41eae81827
Andrijana <andrijanagj96@gmail.com>
2022-06-26 11:13:41 +0200

    create random users section to follow

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from "../components/Feed";
 import Sidebar from "../components/sidebar";
 import Widgets from "../components/Widgets";
 
-export default function Home({ newsResults }) {
+export default function Home({ newsResults, randomUsersResults }) {
   return (
     <div>
       <Head>
@@ -20,7 +20,10 @@ export default function Home({ newsResults }) {
         <Feed />
 
         {/*Widgets */}
-        <Widgets newsResults={newsResults.articles} />
+        <Widgets
+          newsResults={newsResults.articles}
+          randomUsersResults={randomUsersResults.results}
+        />
       </main>
     </div>
   );
@@ -32,9 +35,16 @@ export async function getServerSideProps() {
   const newsResults = await fetch(
     "https://saurav.tech/NewsAPI/top-headlines/category/sports/us.json"
   ).then((res) => res.json());
+
+  //who to follow section
+
+  const randomUsersResults = await fetch(
+    "https://randomuser.me/api/?results=30&inc=name,login,picture"
+  ).then((res) => res.json());
   return {
     props: {
       newsResults,
+      randomUsersResults,
     },
   };
 }
