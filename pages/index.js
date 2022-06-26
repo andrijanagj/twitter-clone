743c62fdd0e904845de4d18b1333911676db5528
Andrijana <andrijanagj96@gmail.com>
2022-06-26 10:43:49 +0200

    create the news section

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from "../components/Feed";
 import Sidebar from "../components/sidebar";
 import Widgets from "../components/Widgets";
 
-export default function Home() {
+export default function Home({ newsResults }) {
   return (
     <div>
       <Head>
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen max-w-7xl mx-auto">
+      <main className="flex min-h-screen  mx-auto">
         {/*Sidebar */}
         <Sidebar></Sidebar>
         {/*feed */}
@@ -20,8 +20,21 @@ export default function Home() {
         <Feed />
 
         {/*Widgets */}
-        <Widgets />
+        <Widgets newsResults={newsResults.articles} />
       </main>
     </div>
   );
+}
+
+//https://saurav.tech/NewsAPI/top-headlines/category/sports/us.json
+
+export async function getServerSideProps() {
+  const newsResults = await fetch(
+    "https://saurav.tech/NewsAPI/top-headlines/category/sports/us.json"
+  ).then((res) => res.json());
+  return {
+    props: {
+      newsResults,
+    },
+  };
 }
