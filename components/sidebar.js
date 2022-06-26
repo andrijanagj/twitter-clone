aece29fd5bd662fb688e7e64e82c7f96d3dc0bd0
Andrijana <andrijanagj96@gmail.com>
2022-06-26 15:07:03 +0200

    get session and modify sidebar and input sections

--- components/sidebar.js
+++ components/sidebar.js
@@ -11,8 +11,9 @@ import {
   InboxIcon,
   UserIcon,
 } from "@heroicons/react/outline";
-
+import { useSession, signIn, signOut } from "next-auth/react";
 function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
       {/*Logo */}
@@ -27,33 +28,59 @@ function Sidebar() {
       <div className="mt-4 mb-2.5 xl:items-start">
         <SidebarMenuItem text="Home" Icon={HomeIcon} active></SidebarMenuItem>
         <SidebarMenuItem text="Explore" Icon={HashtagIcon}></SidebarMenuItem>
-        <SidebarMenuItem text="Notifications" Icon={BellIcon}></SidebarMenuItem>
-        <SidebarMenuItem text="Messages" Icon={InboxIcon}></SidebarMenuItem>
-        <SidebarMenuItem text="Bookmark" Icon={BookmarkIcon}></SidebarMenuItem>
-        <SidebarMenuItem text="Lists" Icon={ClipboardIcon}></SidebarMenuItem>
-        <SidebarMenuItem text="Profile" Icon={UserIcon}></SidebarMenuItem>
-        <SidebarMenuItem
-          text="More"
-          Icon={DotsCircleHorizontalIcon}
-        ></SidebarMenuItem>
+        {session && (
+          <>
+            <SidebarMenuItem
+              text="Notifications"
+              Icon={BellIcon}
+            ></SidebarMenuItem>
+            <SidebarMenuItem text="Messages" Icon={InboxIcon}></SidebarMenuItem>
+            <SidebarMenuItem
+              text="Bookmark"
+              Icon={BookmarkIcon}
+            ></SidebarMenuItem>
+            <SidebarMenuItem
+              text="Lists"
+              Icon={ClipboardIcon}
+            ></SidebarMenuItem>
+            <SidebarMenuItem text="Profile" Icon={UserIcon}></SidebarMenuItem>
+            <SidebarMenuItem
+              text="More"
+              Icon={DotsCircleHorizontalIcon}
+            ></SidebarMenuItem>
+          </>
+        )}
       </div>
 
       {/*Button */}
-      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
-        Tweet
-      </button>
-      {/*mini profile */}
-      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto ">
-        <img
-          className="h-10 w-10 rounded-full xl:mr-2"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/800px-Circle-icons-profile.svg.png"
-        ></img>
-        <div className="leading-5 hidden xl:inline">
-          <h4 className="font-bold">Andrijana Gjokaj</h4>
-          <p className="text-gray-500">@andrijanan</p>
-        </div>
-        <DotsHorizontalIcon className="h-5 xl:ml-9 hidden xl:inline" />
-      </div>
+      {session ? (
+        <>
+          {" "}
+          <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+            Tweet
+          </button>
+          {/*mini profile */}
+          <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto ">
+            <img
+              onClick={signOut}
+              className="h-10 w-10 rounded-full xl:mr-2"
+              src={session.user.image}
+            ></img>
+            <div className="leading-5 hidden xl:inline">
+              <h4 className="font-bold">{session.user.name}</h4>
+              <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <DotsHorizontalIcon className="h-5 xl:ml-9 hidden xl:inline" />
+          </div>
+        </>
+      ) : (
+        <button
+          onClick={signIn}
+          className="bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline"
+        >
+          Sign in
+        </button>
+      )}
     </div>
   );
 }
